767310895fa19a1fed9dc4c7540e9a3b614f9e5f
Moin Patel <moinpatel1992@gmail.com>
2022-03-05 16:01:27 -0500

    https added to api link

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import { useState, useEffect } from 'react';
 import './App.css'
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
-// 27031439
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=27031439';
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=27031439';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
